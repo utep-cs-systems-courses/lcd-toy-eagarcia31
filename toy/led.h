b91fe524701ccf1b1ec62d2e4f42699cfbeeb448
eagarcia31 <eagarcia31@miners.utep.edu>
2022-11-21 12:56:13 -0700

    Make and h updated

---
+++ toy/led.h
@@ -0,0 +1,16 @@
+#ifndef led_included
+#define led_included
+
+#include <msp430.h>
+
+#define LED_RED BIT0
+#define LED_GREEN BIT6
+#define LEDS (BIT0 | BIT6)
+
+extern unsigned char red_on, green_on;
+extern unsigned char leds_changed, green_led_state, red_led_state;
+
+void led_init();
+void led_update();
+
+#endif
